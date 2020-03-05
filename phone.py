b0399f72a29d1a3a39deef767fd67ed7ae08d5bf
Ivan Shcherbakov <uuwaan@yandex.ru>
2020-03-06 00:11:44 +0300

    feat: implement phone Book class

--- phone.py
+++ phone.py
@@ -20,3 +20,32 @@ class Contact:
                 for key, value in self.extras.items()
             ))
         return "\n".join(str_items)
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+        self._contacts = []
+
+    def add(self, contact):
+        if contact not in self._contacts:
+            self._contacts.append(contact)
+
+    def del_by_phone(self, phone):
+        self._contacts = [c for c in self._contacts if c.phone != phone]
+
+    def all_favs(self):
+        return (c for c in self._contacts if c.fav)
+
+    def all_contacts(self, first_name=None, last_name=None):
+        def matches(c):
+            first_m = first_name is None or c.first_name == first_name
+            secnd_m = last_name is None or c.last_name == last_name
+            return first_m and secnd_m
+        return (c for c in self._contacts if matches(c))
+
+    def __str__(self):
+        str_items = []
+        str_items.append("Телефонная книга '{0}':".format(self.name))
+        str_items.extend(str(c) for c in self._contacts)
+        return "\n\n".join(str_items)
